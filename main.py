1d2d5712623cea69e4b8aa0f72773e428b6c74c0
Thiago Rodrigo <throdrigoms@gmail.com>
2026-03-17 20:58:25 -0300

    feat: function to sum two numbers

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def hello_world():
     print("hello world")
 
+def sum(first_number: float, second_number: float) -> None:
+    print(f"the sum of {first_number} + {second_number} is {first_number + second_number}")
 
 def main():
     hello_world()
+    sum(1, 5)
 
 if __name__ == "__main__":
     main()
