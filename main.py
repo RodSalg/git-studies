f4e222a302eadf94b9001152bd4645862800b29b
Thiago Rodrigo <throdrigoms@gmail.com>
2026-03-17 21:10:21 -0300

    feat: function to multiply two numbers

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ def hello_world():
 def sum(first_number: float, second_number: float) -> None:
     print(f"the sum of {first_number} + {second_number} is {first_number + second_number}")
 
+def multiplication_two_numbers(first_number: float, second_number: float) -> None:
+    print(f"the product of {first_number} times {second_number} is {first_number * second_number}")
+
 def main():
     hello_world()
     sum(1, 5)
+    multiplication_two_numbers(2, 10)
 
 if __name__ == "__main__":
     main()
